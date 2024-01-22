c890e314b4e0385a6963ef259331c5f57dfd3075
eneamilo514 <eneamilo514@gmail.com>
2024-01-22 10:09:23 -0800

    added file

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@
 def print_last_digit(number):
     if number < 0:
         number = abs(number)
-        last_digit = number % 10 * -1
+        last_digit = number % 10
         return last_digit
     last_digit = number % 10
     return last_digit
