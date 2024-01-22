4c16903ad03982b1a72c7fe693f11fb696e5b8cc
eneamilo514 <eneamilo514@gmail.com>
2024-01-22 15:03:49 -0800

    added file

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b > 0:
+        res = 1
+        for i in range (b):
+            res *= a
+        return res
+    else:
+        res = 1
+        for i in range (-b):
+            res *= (1/a)
+        return res
