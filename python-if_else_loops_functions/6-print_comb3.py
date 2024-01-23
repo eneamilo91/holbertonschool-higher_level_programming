f661008460e48370908b0d9e6847aa5db68da0f8
eneamilo514 <eneamilo514@gmail.com>
2024-01-23 02:33:26 -0800

    added file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    print_last_digit = abs(number) % 10
-    print(print_last_digit, end="")
-    return print_last_digit
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            if i != 8 and i != 9:
+                print("{}{}, ".format(i, j), end="")
+            else:
+                print("{}{}".format(i, j), end="")
