1d908149b1c478105a427fb6c3f6f3375ec8f780
eneamilo514 <eneamilo514@gmail.com>
2024-01-24 12:56:09 -0800

    added file

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for number in matrix[i]:
+            print("{:d} ".format(number), end="")
+        print()
