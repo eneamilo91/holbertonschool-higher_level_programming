831a86fcd6b784adaf442e8e09027df515cc6570
eneamilo514 <eneamilo514@gmail.com>
2024-01-24 13:49:18 -0800

    added file

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -14,6 +14,18 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
         new_tuple.append(tuple_a[0] + tuple_b[0])
         new_tuple.append(0)
+    elif len(tuple_a) == 2 and len(tuple_b) == 0:
+        new_tuple.append(tuple_a[0])
+        new_tuple.append(tuple_a[1])
+    elif len(tuple_a) == 0 and len(tuple_b) == 2:
+        new_tuple.append(tuple_b[0])
+        new_tuple.append(tuple_b[1])
+    elif len(tuple_a) == 2 and len(tuple_b) == 1:
+        new_tuple.append(tuple_a[0] + tuple_b[0])
+        new_tuple.append(tuple_a[1])
+    elif len(tuple_a) == 1 and len(tuple_b) == 2:
+        new_tuple.append(tuple_a[0] + tuple_b[0])
+        new_tuple.append(tuple_b[1])
     else:
         new_tuple.append(tuple_a[0] + tuple_b[0])
         new_tuple.append(tuple_a[1] + tuple_b[1])
