4285b84b38fa0fedea062f628fca544fc747b3e7
eneamilo514 <eneamilo514@gmail.com>
2024-01-25 08:44:31 -0800

    added file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d} ".format(matrix[i][j]), end="")
+    for row in matrix:
+        for number in row:
+            print("{:d}".format(number), end="")
+            if row.index(number) != len(row) - 1:
+                print(" ".format(), end="")
+        print()
