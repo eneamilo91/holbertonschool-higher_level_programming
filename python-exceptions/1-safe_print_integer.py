3c89e72f44c68d747f00e9c065efdec0626e7843
eneamilo514 <eneamilo514@gmail.com>
2024-02-01 03:18:39 -0800

    added file

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            print()
-            return True
-    except TypeError:
-        print("is not an integer")
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
