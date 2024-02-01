6c0164af6bfbfbb36ddd99d236cae9f464f97a56
eneamilo514 <eneamilo514@gmail.com>
2024-02-01 04:44:25 -0800

    added file

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    nr_elem = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                nr_elem += 1
+            else:
+                continue
+        except IndexError:
+            break
+    print()
+    return nr_elem
