25fe8d109307a19b59da30fe09ec7da709d31ab9
eneamilo514 <eneamilo514@gmail.com>
2024-02-01 05:10:03 -0800

    added file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 nr_elem += 1
             else:
                 continue
-        except:
+        except IndexError
             raise IndexError
             break
     print()
