34cd092bba588a2fc58d866830a7bb5ff85a3285
eneamilo514 <eneamilo514@gmail.com>
2024-02-01 02:52:23 -0800

    added file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    nr_elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            nr_elements += 1
+        except Indexerror:
+            break
+    print()
+    return nr_elements
