83b5a849dde7c72bed6cb789425a07fad79940d3
eneamilo514 <eneamilo514@gmail.com>
2024-02-01 19:10:50 -0800

    added file

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,37 +1,80 @@
 #!/usr/bin/python3
+'''
 
-"""module about a class in python"""
+Simple python file for createing a Square class
+and its initiliazation with one private instance attribute
+'''
 
 
 class Square:
-    """the class Square"""
+    '''
+
+    Square class
+    '''
+
     def __init__(self, size=0, position=(0, 0)):
-        """func to initialize objects"""
-        self.__size = size
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        '''
 
-        self.__position = position
+        Init method for square instances
+        Args:
+            size (int): Size of the square
+        '''
+        self.size = size
+        self.position = position
 
-    def area(self):
-        """Method to calculate the area of the square"""
-        return self.__size ** 2
+    '''
+
+    Property setter for attribute size
+    '''
 
     @property
     def size(self):
-        """getter of the property"""
         return self.__size
+
+    '''
+    Property decorator for getting position attrbiute
+    '''
+
+    @property
+    def position(self):
+        return self.__position
+
+    '''
+    Property decorator for setting position attribute
+    '''
+
+    @position.setter
+    def position(self, value):
+        if (isinstance(value, tuple) is False or len(value) != 2
+           or (isinstance(value[0], int) is False or value[0] < 0)
+           or (isinstance(value[1], int) is False or value[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    '''
+    Property decorator for setting size attribute
+    '''
 
     @size.setter
     def size(self, value):
-        """methd to set the value of an attribute"""
-        if not isinstance(value, int):
+        if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("size must be >=0")
         self.__size = value
+
+    '''
+
+    Public instance method to calculate the area of the instance square
+    '''
+
+    def area(self):
+        return self.size ** 2
+
+    '''
+
+    Instance method to print the area of the square
+    '''
 
     def my_print(self):
         if self.size == 0:
@@ -45,16 +88,3 @@ class Square:
                 for b in range(self.size):
                     print("#", end="")
                 print()
-
-    @property
-    def position(self):
-        """Method to retrieve position"""
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if (isinstance(value, tuple) is False or len(value) != 2
-           or (isinstance(value[0], int) is False or value[0] < 0)
-           or (isinstance(value[1], int) is False or value[1] < 0)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
