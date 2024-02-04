b0c0507a16a90af040ce6cd9db8cf30ab1530ea7
eneamilo514 <eneamilo514@gmail.com>
2024-02-03 17:55:01 -0800

    added file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,15 +6,8 @@ def roman_to_int(roman_string):
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = 0
     for i in range(len(roman_string)):
-        if i == len(roman_string) - 1:
+        if i > 0 and my_dict[roman_string[i]] > my_dict[roman_string[i - 1]]:
+            sum += my_dict[roman_string[i]] - 2 * my_dict[roman_string[i - 1]]
+        else:
             sum += my_dict[roman_string[i]]
-            break
-        if roman_string[i] == "I":
-            if roman_string[i + 1] != "I":
-                sum -= 1
-                continue
-            else:
-                sum += 1
-                continue
-        sum += my_dict[roman_string[i]]
     return sum
