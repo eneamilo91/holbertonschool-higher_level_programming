746405aa14e08d5e74c4fd4bbef2ecab0d568642
eneamilo91 <eneamilo514@gmail.com>
2024-02-07 13:29:21 +0000

    added file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module to sort a list"""
+
+
+class Mylist(list):
+    """Class to perform specific operation"""
+    def print_sorted(self):
+        """Func to sort list"""
+        print(sorted(self))
