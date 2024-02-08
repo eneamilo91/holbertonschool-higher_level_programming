a39574b03ce6260503f859118ea838fbf41ccc5e
eneamilo91 <eneamilo514@gmail.com>
2024-02-08 10:33:01 +0000

    added file

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """Module about a square"""
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
@@ -8,8 +8,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Class that implements a square"""
     def __init__(self, size):
-        super().__init__(size, size)
         Rectangle.integer_validator(self, "size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
