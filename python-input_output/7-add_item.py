136bfbab6211283b0f3193541d489ae81d6b6d3b
eneamilo91 <eneamilo514@gmail.com>
2024-02-11 03:03:24 +0000

    added file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if os.exists("add_item.json"):
+if os.path.exists("add_item.json"):
     list1 = load_from_json_file("add_item.json")
 list1 = []
 for i in range(1, len(argv)):
-    list1.append(argv)
+    list1.append(argv[i])
 save_to_json_file(list1, "add_item.json")
