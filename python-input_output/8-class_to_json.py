804632ebae22f2474dbc5cd375a295a3f6d0579b
eneamilo91 <eneamilo514@gmail.com>
2024-02-11 19:07:43 +0000

    added file

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 """module to perform action"""
-import json
 
 
 def class_to_json(obj):
     """func to convert to json"""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
