08ee601b4e6ebed1b5056d8d6e99576ad0f63145
eneamilo91 <eneamilo514@gmail.com>
2024-02-12 21:54:50 +0000

    added file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         if id is None:
-            __nb_objects += 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
         else:
             self.id = id
