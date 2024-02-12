bf996407d168612be70dff60e49cc215909c635f
eneamilo91 <eneamilo514@gmail.com>
2024-02-12 23:14:08 +0000

    added file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        return f"[Rectangle] ({self.__id}) {self.__x}/{self.__y}\
+        - {self.__width}/{self.__height}"
