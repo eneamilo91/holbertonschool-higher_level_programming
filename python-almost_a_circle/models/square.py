3fec6231315860aed61a8050c5be2419aa1e43fd
eneamilo91 <eneamilo514@gmail.com>
2024-02-13 17:20:38 +0000

    added file

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,3 +11,12 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
+
+    @property
+    def size(self):
+        return self.__height
+
+    @size.setter
+    def size(self, value):
+        self.__width = value
+        self.__height = value
