2d948631025116c6a9957aa35a15b516be918799
eneamilo91 <eneamilo514@gmail.com>
2024-02-13 11:53:47 +0000

    added file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,16 @@ class Rectangle(Base):
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
  - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """func to add args to object"""
+        if len(args) >= 1:
+            self.__id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
