d9b74c63c80efd348a58dcf80b13b23d9feaef1a
eneamilo91 <eneamilo514@gmail.com>
2024-02-13 17:36:36 +0000

    added file

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,3 +20,20 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """func to add args to object"""
+        attributes = ["id", "height", "x", "y"]
+        if args and args is not None:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.height = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key in kwargs.keys():
+                if key in attributes:
+                    setattr(self, key, kwargs[key])
