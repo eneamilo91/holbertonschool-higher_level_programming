ba1e96ee986b2c2a249d3cfaa4858574d391f1c8
eneamilo91 <eneamilo514@gmail.com>
2024-02-13 17:45:26 +0000

    added file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,13 +83,13 @@ class Rectangle(Base):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.__width = args[1]
+                self.width = args[1]
             if len(args) >= 3:
-                self.__height = args[2]
+                self.height = args[2]
             if len(args) >= 4:
-                self.__x = args[3]
+                self.x = args[3]
             if len(args) >= 5:
-                self.__y = args[4]
+                self.y = args[4]
         else:
             for key in kwargs.keys():
                 if key in attributes:
