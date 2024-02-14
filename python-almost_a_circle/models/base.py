aa991712b4045674083b2c6cec1d4dd9248ccc88
eneamilo91 <eneamilo514@gmail.com>
2024-02-14 00:03:06 +0000

    added file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,20 @@ class Base:
             return "[]"
         my_list = json.dumps(list_dictionaries)
         return my_list
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        Class method to save json represantation of
+        object files
+        '''
+
+        with open(f"{cls.__name__}.json", "w+") as file_js:
+            if list_objs is None:
+                file_js.write(json.dumps([]))
+                return
+            list_of_dictionaries = []
+            for objs in list_objs:
+                if issubclass(objs.__class__, Base):
+                    list_of_dictionaries.append(objs.to_dictionary())
+            file_js.write(Base.to_json_string(list_of_dictionaries))
