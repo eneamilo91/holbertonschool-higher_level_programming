d35a3b98ac05a0576aa5f003cbefabdd0ad4458a
eneamilo91 <eneamilo514@gmail.com>
2024-03-19 09:58:01 +0000

    added file

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- script that creates a table
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (1, "John", 10);
-INSERT INTO second_table (2, "Alex", 3);
-INSERT INTO second_table (3, "BoB", 14);
-INSERT INTO second_table (4, "George", 8);
+INSERT INTO second_table VALUES(1, "John", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "BoB", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
