047047c08d1294dfd78f6a69d3876f206b06fd22
eneamilo91 <eneamilo514@gmail.com>
2024-03-21 20:14:23 +0000

    added file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,6 +3,6 @@ mysql -u root -p'root'
 
 USE mysql;
 
-SELECT * FROM user WHERE User='user_0d_1' AND Host='localhost';
+SELECT * FROM user WHERE User='user_0d_1' AND Host='root';
 
-SELECT * FROM user WHERE User='user_0d_2' AND Host='localhost';
+SELECT * FROM user WHERE User='user_0d_2' AND Host='root';
